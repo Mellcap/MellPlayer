95cfb090bcb4058a8c36887004d587d479739c78
Mellcap <mellcap@163.com>
2017-02-16 18:03:00 +0800

    initial

---
+++ MellPlayer/api.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Netease Music API
+'''
+import requests
+
+class Netease(object):
+
+    def __init__(self):
+        self.playlist_categories = []
