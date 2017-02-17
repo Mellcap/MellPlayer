368d3b6f1a01052db7f1eab8d045d81f3f6ee45f
Mellcap <mellcap@163.com>
2017-02-17 16:07:50 +0800

    standard

--- MellPlayer/api.py
+++ MellPlayer/api.py
@@ -11,27 +11,46 @@ class Netease(object):
     def __init__(self):
         self.playlist_categories = []
 
-    # 分类歌单
-    # http://music.163.com/discover/playlist/
     def playlist_categories(self):
+        '''
+        分类歌单
+        http://music.163.com/discover/playlist/
+        '''
         pass
 
-    # 分类详情
-    # http://music.163.com/api/playlist/list?cat=学习&order=hot&offset=0&total=false&limit=50
     def playlist_category_detail(self):
+        '''
+        分类详情
+        http://music.163.com/api/playlist/list?cat=学习&order=hot&offset=0&total=false&limit=50
+        '''
         pass
 
-    # 歌单详情
-    # http://music.163.com/api/playlist/detail?id=xxx
     def playlist_detail(self):
+        '''
+        歌单详情
+        http://music.163.com/api/playlist/detail?id=xxx>
+        '''
         pass
 
-    # 歌曲详情
-    # http://music.163.com/api/song/detail?ids=[xxx, xxx]
+    
     def song_detail(self):
+        '''
+        歌曲详情
+        http://music.163.com/api/song/detail?ids=[xxx, xxx]
+        '''
         pass
 
-    # 歌词详情
-    # http://music.163.com/api/song/lyric?os=osx&id=xxx&lv=-1&kv=-1&tv=-1
+   
     def lyric_detail(self):
+        '''
+        歌词详情
+        http://music.163.com/api/song/lyric?os=osx&id=xxx&lv=-1&kv=-1&tv=-1
+        '''
+        pass
+
+
+    def parse_info(self, data, type):
+        '''
+        解析信息
+        '''
         pass
