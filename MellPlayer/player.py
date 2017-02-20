63ae0b619ea50b1e234abc139becaeb84c703302
Mellcap <mellcap@163.com>
2017-02-20 10:47:49 +0800

    add player class

---
+++ MellPlayer/player.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Netease Music Player
+
+Created on 2017-02-20
+@author: Mellcap
+'''
+
+
+class Player(object):
+
+    def __init__(self):
+        pass
+
+    def start(self):
+        pass
+
+    def pause(self):
+        pass
+
+    def start_or_pause(self):
+        pass
+
+    def switch_song(self, action='next'):
+        '''
+        action: next/prev
+        '''
+        pass
+
+    def switch_playlist(self, action='next'):
+        '''
+        action: next/prev
+        '''
+        pass
