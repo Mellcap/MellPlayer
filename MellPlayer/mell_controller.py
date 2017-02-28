f6e6c10fe3a83be491eae7d1b675be0f49e639b6
Mellcap <mellcap@163.com>
2017-02-28 13:13:56 +0800

    add key_watcher

--- MellPlayer/mell_controller.py
+++ MellPlayer/mell_controller.py
@@ -10,6 +10,7 @@ Created on 2017-02-21
 
 import threading
 import time
+import queue
 
 import getch
 import ui
@@ -36,27 +37,45 @@ CONFIG = {
 def my_log(loglevel, component, message):
     if loglevel == 'error':
         print('>>>>> I got an error')
-        # print('>>>>> player: %s' % mell_player.playlist)
-        # mell_player.pause = True
-        # mell_player.next_song()
     print('[{}] {}: {}'.format(loglevel, component, message))
 
 mell_ui = ui.UI()
 mell_player = player.Player(log_handler=my_log, ytdl=True)
+q = queue.Queue()
 
-def watch_key():
+def watcher():
     while 1:
         key = getch.getch()
-        # print('key:%s' % key)
-        action = CONFIG.get(key, None)
-        if action == 'quit':
+        q.put(key)
+        if key == 'q':
             break
-        elif action:
-            try:
-                func = 'handler_%s' % action
-                eval(func)()
-            except Exception:
-                pass
+
+def executor():
+    while 1:
+        try:
+            key = q.get_nowait()
+            action = CONFIG.get(key, None)
+            if action == 'quit':
+                break
+            elif action:
+                try:
+                    func = 'handler_%s' % action
+                    eval(func)()
+                except Exception:
+                    pass
+        except Exception:
+            pass
+
+
+def key_watcher():
+    t1 = threading.Thread(target=watcher)
+    t2 = threading.Thread(target=executor)
+
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+
 
 def handler_next_line():
     mell_ui.next_line()
@@ -85,32 +104,30 @@ def handler_lyric():
 def handler_help():
     pass
 
-def run():
-    p = '/Users/zhaoye/.MellPlayer/playlist.m3u'
-    mell_player.loadlist(p)
-    mell_player.wait_for_playback()
-    print('wait 5 seconds')
-    time.sleep(5)
-    mell_player.pause = True
-    print('wait another 5 seconds')
-    time.sleep(5)
-    mell_player.pause = False
+# def run():
+#     p = '/Users/zhaoye/.MellPlayer/playlist.m3u'
+#     mell_player.loadlist(p)
+#     mell_player.wait_for_playback()
+#     print('wait 5 seconds')
+#     time.sleep(5)
+#     mell_player.pause = True
+#     print('wait another 5 seconds')
+#     time.sleep(5)
+#     mell_player.pause = False
+#     print('wait another 5 seconds')
+#     time.sleep(5)
+#     mell_player.play('http://m2.music.126.net/LJOP6drGL9Vo2cmkjbFazQ==/3393092919088491.mp3')
 
 
 if __name__ == '__main__':
-    # mell_ui.display()
-    # watch_key()
+    mell_ui.display()
+    key_watcher()
     # create_directory()
     # mell_player.category = '流行'
     # mell_player.get_category_playlists()
     # mell_player.run_playlist()
-    t = threading.Thread(target=run)
-    t.start()
-
-    # mell_player.playlist_next()
-
-
-
+    # t = threading.Thread(target=run)
+    # t.start()
 
     
 
